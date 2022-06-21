0c92b207e7dc6e61bc9d3304fbffc99ef14a7fad
Josh <josh.haddix598@gmail.com>
2022-06-21 18:58:38 -0500

    added drag-drop functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -180,6 +180,71 @@ $("#remove-tasks").on("click", function() {
   saveTasks();
 });
 
+$(".card .list-group").sortable({
+  connectWith: $(".card .list-group"),
+  scroll: false,
+  tolerance: "pointer",
+  helper: "clone",
+
+  activate: function(event){
+    
+  },
+  deactivate: function(event){
+    
+  },
+  over: function(event){
+    
+  },
+  out: function(event){
+    
+  },
+  update: function(event) {
+    var tempArr = [];
+    $(this).children().each(function() {
+      
+
+      var text = $(this)
+        .find("p")
+        .text()
+        .trim();
+
+      var date = $(this)
+        .find("span")
+        .text()
+        .trim();
+
+      
+      tempArr.push({
+        text:text,
+        date:date
+      });
+    });
+    // trim down lists's ID to match object property
+    var arrName = $(this)
+      .attr("id")
+      .replace("list-","");
+    
+    // update array on tasks object and save
+    tasks[arrName] = tempArr;
+    saveTasks();
+  }
+});
+
+$("#trash").droppable({
+  accept: ".card .list-group-item",
+  tolerance: "touch",
+  drop: function(event, ui) {
+    ui.draggable.remove();
+  },
+  over: function(event, ui) {
+    console.log("over");
+    // an alert here would force a drop. just, y'know, something to think about.
+  },
+  out: function(event,ui) {
+    console.log("out");
+  }
+})
+
 // load tasks for the first time
 loadTasks();
 
