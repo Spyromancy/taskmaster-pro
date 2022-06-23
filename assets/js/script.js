f4ce12ad6b5e785858753ce409e5aa1cb8f94b2e
Josh <josh.haddix598@gmail.com>
2022-06-23 17:29:54 -0500

    UX enhancements

--- assets/js/script.js
+++ assets/js/script.js
@@ -183,7 +183,7 @@ $("#task-form-modal").on("shown.bs.modal", function() {
 });
 
 // save button in modal was clicked
-$("#task-form-modal .btn-primary").click(function() {
+$("#task-form-modal .btn-save").click(function() {
   // get form values
   var taskText = $("#modalTaskDescription").val();
   var taskDate = $("#modalDueDate").val();
@@ -224,16 +224,18 @@ $(".card .list-group").sortable({
   helper: "clone",
 
   activate: function(event){
-    
+    $(this).addClass("dropover");
+    $(".bottom-trash").addClass("bottom-trash-drag");
   },
   deactivate: function(event){
-    
+    $(this).removeClass("dropover");
+    $(".bottom-trash").removeClass("bottom-trash-drag");
   },
   over: function(event){
-    
+    $(this).addClass("dropover-active");
   },
   out: function(event){
-    
+    $(this).removeClass("dropover-active");
   },
   update: function(event) {
     var tempArr = [];
@@ -272,17 +274,24 @@ $("#trash").droppable({
   tolerance: "touch",
   drop: function(event, ui) {
     ui.draggable.remove();
+    $(".bottom-trash").removeClass("bottom-trash-active");
   },
   over: function(event, ui) {
-    console.log("over");
+    $(".bottom-trash").addClass("bottom-trash-active");
+
     // an alert here would force a drop. just, y'know, something to think about.
   },
   out: function(event,ui) {
-    console.log("out");
+    $(".bottom-trash").removeClass("bottom-trash-active");
   }
 })
 
 // load tasks for the first time
 loadTasks();
+setInterval(function(){
+  $(".card .list-group-item").each(function(index, el) {
+    auditTask(el);
+  });
+}, (1000 * 60) * 30);
 
 
